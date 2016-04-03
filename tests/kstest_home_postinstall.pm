dd247c7737e729922f1895f5ddfd9e5d74ff90e6
Adam Williamson <awilliam@redhat.com>
2016-04-02 17:27:11 -0700

    add bits for translating anaconda kickstart_tests to openQA
    perl2json converts the openQA templates file to JSON (so we
    can parse it with Python). kstest-converter does the meat of
    the work of translating kickstart-tests to openQA tests: its
    'kickstarts' subcommand produces .ks files from the .ks.in
    files (you need to then make them available via HTTP somehow),
    its 'templates' subcommand produces openQA 'test suites' and
    'job templates' and merges them into an existing templates
    file. Then you can load the templates file and fire off the
    tests.

---
+++ tests/kstest_home_postinstall.pm
@@ -0,0 +1,23 @@
+use base "installedtest";
+use strict;
+use testapi;
+
+sub run {
+    my $self=shift;
+    if (not( check_screen "root_console", 0)) {
+        $self->root_console(tty=>3);
+    }
+    validate_script_output 'cat /home/RESULT', sub { $_ =~ m/SUCCESS/ };
+}
+
+sub test_flags {
+    # without anything - rollback to 'lastgood' snapshot if failed
+    # 'fatal' - whole test suite is in danger if this fails
+    # 'milestone' - after this test succeeds, update 'lastgood'
+    # 'important' - if this fails, set the overall state to 'fail'
+    return { fatal => 1 };
+}
+
+1;
+
+# vim: set sw=4 et:
