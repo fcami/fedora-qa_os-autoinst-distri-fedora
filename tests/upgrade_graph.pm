efa70c86fb28a489c8efb0cecc00b851939ccc6b
Lukas Ruzicka <lruzicka@redhat.com>
2018-06-27 09:49:09 +0200

    Clean dnf stuff.

--- tests/upgrade_graph.pm
+++ tests/upgrade_graph.pm
@@ -7,36 +7,12 @@ sub run {
     my $self = shift;
     my $release = lc(get_var("VERSION"));
     my $relnum = $release;
-
-    $self->root_console(tty=>3);
-
     if ($release eq "rawhide") {
         $relnum = get_var("RAWREL", "rawhide");
     }
     # disable screen blanking (download can take a long time)
     script_run "setterm -blank 0";
 
-    # use compose repo (compose tests) or set up update repo (update tests)
-    repo_setup();
-    my $params = "-y --releasever=${relnum}";
-    if ($release eq "rawhide") {
-        $params .= " --nogpgcheck";
-    }
-
-    if (script_run "dnf ${params} system-upgrade download", 6000) {
-        record_soft_failure "dnf failed so retry with --allowerasing";
-        $params .= " --allowerasing";
-        assert_script_run "dnf ${params} system-upgrade download", 6000;
-    }
-
-    upload_logs "/var/log/dnf.log";
-    upload_logs "/var/log/dnf.rpm.log";
-
-    script_run "dnf system-upgrade reboot", 0;
-    # fail immediately if we see a DNF error message, but keep an eye
-    # out for the bootloader so we can handle it if requested
-    check_screen ["upgrade_fail", "bootloader"], 15;
-    die "DNF reported failure" if (match_has_tag "upgrade_fail");
 
     # handle bootloader, if requested; set longer timeout as sometimes
     # reboot here seems to take a long time
